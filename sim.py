001ced6022297e0ed77f7f4f94be73555c196f34
mfekadu <mike.fekadu@gmail.com>
2019-08-27 22:38:39 -0400

    simple pygame hello world

--- sim.py
+++ sim.py
@@ -1,3 +1,22 @@
 #!/usr/bin/env python3
 
-print("hello world")
+import pygame
+
+# game width and height
+GW = 800
+GH = 800
+
+def main():
+    pygame.init()
+    DISPLAY = pygame.display.set_mode((GW, GH))
+    CLOCK = pygame.time.Clock()
+
+    # while the game is running
+    while True:
+        print(1)
+    #pygame.quit()
+    #quit()
+
+if __name__ == "__main__":
+    print("hello world")
+    main()
