c41d60b3ca5b8404cd4724e6ad8c66301eb82a3a
mfekadu <mike.fekadu@gmail.com>
2019-08-28 15:07:36 -0400

    switching to pyglet

--- sim.py
+++ sim.py
@@ -1,51 +1,36 @@
 #!/usr/bin/env python3
 
-import pygame # for displaying to screen
+import pyglet # for displaying to screen
 import pymunk # for rigid body physics
-from pymunk.pygame_util import DrawOptions
-import time
+from pymunk.pyglet_util import DrawOptions # idk what this is
 
 # game width and height
 GW = 800
 GH = 800
 
+def setup_window():
+    '''
+    setup the pyglet window
+    '''
+    return pyglet.window.Window(GW, GH, __file__, resizable=True)
+
+def run():
+    '''
+    run the main loop for the game engine
+    '''
+    pyglet.app.run()
+
 def main():
-    pygame.init()
-    DISPLAY = pygame.display.set_mode((GW, GH))
-    CLOCK = pygame.time.Clock()
-    def draw():
-        pass
+    window = setup_window()
+    #options = DrawOptions()
 
-    def update():
-        CLOCK.tick()
+    # setup a Space for pymunk to do the fancy physics
+    #space = pymunk.Space()
+    #space.gravity = (0,-9.8)
 
+    #body = pymunk.Body(1, 1666)
 
-    space = pymunk.Space()
-    space.gravity = 0, -1
-    body = pymunk.Body(1, 1666)
-    body.position = 640, 700
-    poly = pymunk.Poly.create_box(body, size=(50,50))
-    surface = pygame.Surface((10,10))
-    options = DrawOptions(surface)
-#    space.add(body)
-
-#    space.add(body, poly)
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        space.step(0.02) # 1/50
-        print(body.position)
-
-        space.debug_draw(options)
-#        draw()
-#        update()
-
-    pygame.quit()
-    #quit()
+    run()
 
 if __name__ == "__main__":
     print("hello world")
