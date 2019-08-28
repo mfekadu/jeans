d9f2889fd324cbb3b443cb9c7e93f4dc6500b6ed
mfekadu <mike.fekadu@gmail.com>
2019-08-28 14:28:10 -0400

    trying to add pymunk to pygame
    strongly considering a switch to pyglet because perhaps pygame is old and slow? idk... why not let's just try it. they're probably both the same

---
+++ foo.py
@@ -0,0 +1,56 @@
+"""Showcase what the output of pymunk.pygame_util draw methods will look like.
+
+See pyglet_util_demo.py for a comparison to pyglet.
+"""
+
+__docformat__ = "reStructuredText"
+
+import sys
+
+import pygame
+from pygame.locals import *
+    
+import pymunk
+from pymunk.vec2d import Vec2d
+import pymunk.pygame_util
+
+import shapes_for_draw_demos
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1000,700)) 
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont("Arial", 16)
+    
+    space = pymunk.Space()
+    
+    captions = shapes_for_draw_demos.fill_space(space)
+            
+    ### Draw it 
+    screen.fill(pygame.color.THECOLORS["white"])
+    
+    options = pymunk.pygame_util.DrawOptions(screen)
+    space.debug_draw(options)
+    #pymunk.pygame_util.draw(screen, space)
+
+    # Info
+    color = pygame.color.THECOLORS["black"]
+    screen.blit(font.render("Demo example of pygame_util.DrawOptions()", 1, color), (205, 680))
+    for caption in captions:
+        x, y = caption[0]
+        y = 700 - y
+        screen.blit(font.render(caption[1], 1, color), (x,y))
+    pygame.display.flip()
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT or \
+                event.type == KEYDOWN and (event.key in [K_ESCAPE, K_q]):  
+                return 
+            elif event.type == KEYDOWN and event.key == K_p:
+                pygame.image.save(screen, "pygame_util_demo.png")                
+                                  
+        clock.tick(10)
+        
+if __name__ == '__main__':
+    sys.exit(main())
--- sim.py
+++ sim.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-import pygame
+import pygame # for displaying to screen
+import pymunk # for rigid body physics
+from pymunk.pygame_util import DrawOptions
+import time
 
 # game width and height
 GW = 800
@@ -10,12 +13,36 @@ def main():
     pygame.init()
     DISPLAY = pygame.display.set_mode((GW, GH))
     CLOCK = pygame.time.Clock()
+    def draw():
+        pass
+
+    def update():
+        CLOCK.tick()
+
+
+    space = pymunk.Space()
+    space.gravity = 0, -1
+    body = pymunk.Body(1, 1666)
+    body.position = 640, 700
+    poly = pymunk.Poly.create_box(body, size=(50,50))
+    surface = pygame.Surface((10,10))
+    options = DrawOptions(surface)
+#    space.add(body)
+
+#    space.add(body, poly)
 
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+
+        space.step(0.02) # 1/50
+        print(body.position)
+
+        space.debug_draw(options)
+#        draw()
+#        update()
 
     pygame.quit()
     #quit()
