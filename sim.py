2a527273eb4e0b3384783982730fcd17c5b40459
mfekadu <mike.fekadu@gmail.com>
2019-08-28 15:52:06 -0400

    simple window up and running with nothing showing

--- sim.py
+++ sim.py
@@ -8,11 +8,8 @@ from pymunk.pyglet_util import DrawOptions # idk what this is
 GW = 800
 GH = 800
 
-def setup_window():
-    '''
-    setup the pyglet window
-    '''
-    return pyglet.window.Window(GW, GH, __file__, resizable=False)
+# must be global because the @decorators work that way
+window = pyglet.window.Window(GW, GH, __file__, resizable=False)
 
 def run():
     '''
@@ -30,10 +27,45 @@ def schedule(fun):
     '''
     pyglet.clock.schedule_interval(fun, 1.0/60)
 
+def get_pymunk_space():
+    '''returns a `space` where the physics happens'''
+    space = pymunk.Space()
+    # gravity is represented by a tuple 
+    # 0 acceleration in x-axis and -9.8 in y-axis
+    space.gravity = (0,-9.8)
+    return space
+
+def get_pymunk_rigid_body():
+    '''
+    returns a `rigid body` which is a shapeless object that 
+    has physical properties (mass, position, rotation, velocity, etc)
+    '''
+    mass = 1
+    moment_of_inertia = 1666
+    body = pymunk.Body(mass, moment_of_inertia)
+    x = 50
+    y = 100
+    body.position = (x, y)
+    return body
+
+@window.event
+def on_draw():
+    '''
+    stuff that happens when pyglet is drawing
+    '''
+    window.clear() # start with a clean window
 
 def main():
-    window = setup_window()
-    
+    # for no good reason make sure the window is setup
+    assert window
+
+    space = get_pymunk_space()
+
+    body = get_pymunk_rigid_body()
+
+    # OOP better than FP here bc space is big to pass aroudn
+    space.add(body)
+
     # do the update on 1/60th clock-ticks
     schedule(update)
 
