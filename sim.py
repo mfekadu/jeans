b5e7dcbda7c2ef1b03b5ff4979c00c3454d2a377
mfekadu <mike.fekadu@gmail.com>
2019-09-02 16:50:52 -0400

    refactor shape creation functions to be more customizable

--- sim.py
+++ sim.py
@@ -10,7 +10,7 @@ def get_pymunk_space():
     space = pymunk.Space()
     # gravity is represented by a tuple
     # 0 acceleration in x-axis and -9.8 in y-axis
-    space.gravity = (0, -90.8)
+    space.gravity = (0, -9.807)
     return space
 
 
@@ -38,89 +38,87 @@ def schedule(fun):
     pyglet.clock.schedule_interval(fun, 1.0/60)
 
 
-def get_pentagon():
-    mass = 1
-    # vertices are defined counter-clockwise from bottom-left (origin)
-    penta_vertices = ((0, 0), (100, 0), (150, 100), (50, 200), (-50,  100))
-    penta_shape = pymunk.Poly(body=None, vertices=penta_vertices)
-    penta_vertices = penta_shape.get_vertices()
-    penta_moment = pymunk.moment_for_poly(mass=mass, vertices=penta_vertices)
-    penta_body = pymunk.Body(mass=mass, moment=penta_moment)
-    penta_body.position = (700, 800)
-    penta_shape.body = penta_body
-    return penta_body, penta_shape
-
-
-def get_triangle():
+def create_pentagon(p1=(0, 0), p2=(2, 0), p3=(3, 2), p4=(1, 4), p5=(-1, 2),
+                    x=0, y=0, m=1, scalar=1, bt=pymunk.Body.DYNAMIC):
     '''
-    return a triangle
+    given points (p1..p5), mass (m), x-position (x), y-position (y),
+          scalar <to augment default points>, body_type (bt),
+    return (body, shape) tuple for a pentagon
     '''
-    mass = 1
-    triangle_vertices = ((0, 0), (100, 0), (50, 100))  # equilateral
-    triangle_shape = pymunk.Poly(body=None, vertices=triangle_vertices)
-    triangle_vertices = triangle_shape.get_vertices()  # because Vec2d
-    triangle_moment = pymunk.moment_for_poly(mass=mass,
-                                             vertices=triangle_vertices)
-    triangle_body = pymunk.Body(mass=mass, moment=triangle_moment)
-    triangle_body.position = (550, 700)
-    triangle_shape.body = triangle_body  # set here because init None above
-    return triangle_body, triangle_shape
+    vertices = (p1, p2, p3, p4, p5)
+    vertices = tuple((v[0]*scalar, v[1]*scalar) for v in vertices)
+    shape = pymunk.Poly(body=None, vertices=vertices)  # will set body later
+    vertices = shape.get_vertices()  # because Vec2d of vertices is needed
+    moment = pymunk.moment_for_poly(mass=m, vertices=vertices)
+    body = pymunk.Body(mass=m, moment=moment)
+    body.position = (x, y)
+    shape.body = body  # set body here because init None above
+    return body, shape
 
 
-def get_segment():
+def create_triangle(p1=(0, 0), p2=(1, 0), p3=(.5, .866), x=0, y=0,
+                    m=1, scalar=1, bt=pymunk.Body.DYNAMIC):
     '''
-    return a line segment
+    given points (p1..p3), mass (m), x-position (x), y-position (y),
+          scalar <to augment default equilateral triangle>, body_type (bt),
+    The default values for p1,p2,p3 make an approx. equilateral triangle.
+    return (body, shape) tuple for a triangle
     '''
-    point_1 = (0, 0)
-    point_2 = (0, 400)
-    mass = 1
-    thiccness = 2
-    seg_mom = pymunk.moment_for_segment(mass=mass,
-                                        a=point_1,
-                                        b=point_2,
-                                        radius=thiccness)
-    seg_body = pymunk.Body(mass=mass, moment=seg_mom)
-    seg_shape = pymunk.Segment(body=seg_body,
-                               a=point_1,
-                               b=point_2,
-                               radius=thiccness)
-    seg_body.position = (400, 100)
-    return seg_body, seg_shape
+    vertices = (p1, p2, p3)  # equilateral
+    vertices = tuple((v[0]*scalar, v[1]*scalar) for v in vertices)
+    shape = pymunk.Poly(body=None, vertices=vertices)  # will set body later
+    vertices = shape.get_vertices()  # because Vec2d of vertices is needed
+    moment = pymunk.moment_for_poly(mass=m, vertices=vertices)
+    body = pymunk.Body(mass=m, moment=moment)
+    body.position = (x, y)
+    shape.body = body  # set body here because init None above
+    return body, shape
 
 
-def get_pymunk_rigid_circle():
-    mass = 1
-    radius = 70
-    circle_moment = pymunk.moment_for_circle(mass=mass,
-                                             inner_radius=0,
-                                             outer_radius=radius)
-    circle_body = pymunk.Body(mass=mass, moment=circle_moment)
-    circle_shape = pymunk.Circle(body=circle_body, radius=radius)
-    circle_body.position = (400, 400)
-    return circle_body, circle_shape
-
-
-def get_pymunk_rigid_poly():
+def create_segment(p1=(0, 0), p2=(0, 1), thicc=1, x=0, y=0, m=1, scalar=1,
+                   bt=pymunk.Body.DYNAMIC):
     '''
+    given point_1 (p1), point_2 (p2), thickness (thicc),
+          x-position (x), y-position (y), mass (m),
+          scalar <to augment the length>, body_type (bt)
+    return (body, shape) tuple for a line segment
+    '''
+    p2 = (p2[0], p2[1]*scalar)
+    moment = pymunk.moment_for_segment(mass=m, a=p1, b=p2, radius=thicc)
+    body = pymunk.Body(mass=m, moment=moment, body_type=bt)
+    shape = pymunk.Segment(body=body, a=p1, b=p2, radius=thicc)
+    body.position = (x, y)
+    return body, shape
+
+
+def create_circle(r=1, x=0, y=0, m=1, bt=pymunk.Body.DYNAMIC):
+    '''
+    given radius (r), x-position (x), y-position (y), mass (m), body_type (bt)
+    return the (body, shape) tuple for a circle
+    '''
+    moment = pymunk.moment_for_circle(mass=m, inner_radius=0, outer_radius=r)
+    body = pymunk.Body(mass=m, moment=moment)
+    shape = pymunk.Circle(body=body, radius=r)
+    body.position = (x, y)
+    return body, shape
+
+
+def create_rect(w=1, h=1, scalar=1, m=1, x=0, y=0, bt=pymunk.Body.DYNAMIC):
+    '''
+    given the width (w), height (h), mass (m), x-position (x), y-position (y),
+        scalar <to augment default square>, and the body_type (bt).
     returns a `rigid body` which is a shapeless object that
     has physical properties (mass, position, rotation, velocity, etc)
     ALSO returns a Poly which is the Shape that really gets drawn
     '''
-    mass = 1
-    body_type = pymunk.Body.DYNAMIC
-
-    poly_size = (150, 150)
+    poly_size = (w*scalar, h*scalar)
     poly = pymunk.Poly.create_box(body=None, size=poly_size)
     # moment depends on mass and size.
     # bigger poly >> bigger moment.
     # more massive >> bigger moment
-    moment_of_inertia = pymunk.moment_for_poly(mass, poly.get_vertices())
-
-    body = pymunk.Body(mass, moment_of_inertia, body_type)
-    x = 500
-    y = 500
+    moment_of_inertia = pymunk.moment_for_poly(m, poly.get_vertices())
+    body = pymunk.Body(mass=m, moment=moment_of_inertia, body_type=bt)
     body.position = (x, y)
-
     poly.body = body
     return body, poly
 
@@ -139,20 +137,20 @@ def main():
     assert window
     assert space
 
-    body, poly = get_pymunk_rigid_poly()
+    body, poly = create_rect(x=300, y=300, scalar=50)
 
     space.add(body, poly)
 
-    cbody, cshape = get_pymunk_rigid_circle()
+    cbody, cshape = create_circle(r=50, x=400, y=400)
     space.add(cbody, cshape)
 
-    line_body, line_shape = get_segment()
+    line_body, line_shape = create_segment(x=600, y=600, scalar=50)
     space.add(line_body, line_shape)
 
-    tbody, tshape = get_triangle()
+    tbody, tshape = create_triangle(x=100, y=600, scalar=50)
     space.add(tbody, tshape)
 
-    pb, ps = get_pentagon()
+    pb, ps = create_pentagon(x=150, y=150, scalar=50)
     space.add(pb, ps)
 
     # do the update on 1/60th clock-ticks
