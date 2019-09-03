48fba82c006a0f9f1ad1cfe0c03ffd891d535095
mfekadu <mike.fekadu@gmail.com>
2019-09-03 08:35:52 -0400

    Update cfg.py
    cfg.py now has all of the global variables and magic numbers and has been refacrored for readability
    
    new variables:
    ```
    EXIT_SUCCESS = 0
    EXIT_FAILURE = 1
    ```
    
    for doing quit(EXIT_FAILURE)
    
    and expecting
    > echo $?
    to output 1 in case of failure
    
    also moved IGW and IGH to cfg because they are used by rng.py

--- cfg.py
+++ cfg.py
@@ -4,19 +4,52 @@ A place to store global variables to customize and configure the simulation.
 '''
 import colors
 
+
+# ******************************************************************************
+# Constants for random generators
+# ******************************************************************************
 SEED = 42  # set the seed for random number generators for reproducability
 
-DEBUG = True  # optionally set to False/0 (no-debug), 1 (asserts), 2 (prints)
+MOVE_OPTIONS = 5  # move_0, move_up, move_down, move_left, move_right
+
+ITERATOR = 0  # keeps track of how many steps/frames pyglet has looped over
+
+MAX_STEPS = 1000000  # 1 million is the arbitrary limit
+
+
+# ******************************************************************************
+# Constants for engines (pymunk/pyglet)
+# ******************************************************************************
+# parameters of the window
+GW = 400  # game width
+GH = 400  # game height
+BORDER_THICCNESS = 10
+
+
+# the space within the window that food and bots can spawn
+IGW = GW - (BORDER_THICCNESS * 2)  # inner game width
+IGH = GH - (BORDER_THICCNESS * 2)  # inner game height
 
 GRAVITY = (0, 0)
 
+
+# ******************************************************************************
+# Constants for debugging
+# ******************************************************************************
+# DEBUG can optionally be set to:
+#    False/0 (no-debug), 1 (asserts), 2 (prints), 3 (lots of prints)
+DEBUG = True
+
+USAGE = "usage: python3 sim.py [-d [0, 1, 2]]"
+EXIT_SUCCESS = 0
+EXIT_FAILURE = 1
+
+# ******************************************************************************
+# Constants for the simulation (bots, food, etc)
+# ******************************************************************************
 FOOD_COUNT = 100
 FOOD_RADIUS = 5
 FOOD_COLOR = colors.RED
 
-
 BOT_RADIUS = 10
 BOT_COLOR = colors.BLUE
-
-ITERATOR = 0
-MAX_STEPS = 1000000  # 1 million is the arbitrary limit
