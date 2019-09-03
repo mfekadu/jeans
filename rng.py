91ff6264664cbe72cabf24936e462cb1354c1aae
mfekadu <mike.fekadu@gmail.com>
2019-09-03 08:33:39 -0400

    Create rng.py
    rng.py will contain all the necessary random numbers in the appropriate range
    
    cfg.MAX_STEPS is the max range for moves because that will be the end of the simulation

---
+++ rng.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+'''
+rng.py - Random Number Generator
+
+This file will contain randomly generated numpy arrays using cfg.SEED
+    so that the random numbers are consistent throughout each run.
+
+Random numbers are used for making choices in the simulation (e.g move(bot)).
+
+Hopefully setting seed once in this file and generating all the random numbers
+    once in this file, will lead to consistent/predictable results.
+'''
+import cfg
+from numpy import random as np_random
+
+# set the seed for reproducability
+print(np_random.get_state()[1][0], "??", 42)
+np_random.seed(cfg.SEED)
+print(np_random.get_state()[1][0], "??", 42)
+
+# array for the bot moves
+MOVES = np_random.randint(0, cfg.MOVE_OPTIONS, cfg.MAX_STEPS)
+
+# arrays for the food positions
+FOOD_X = np_random.randint(low=cfg.BORDER_THICCNESS, high=cfg.IGW,
+                           size=cfg.FOOD_COUNT)
+FOOD_Y = np_random.randint(low=cfg.BORDER_THICCNESS, high=cfg.IGH,
+                           size=cfg.FOOD_COUNT)
