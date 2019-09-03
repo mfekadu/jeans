6e8ef0decdfbb717b51f56e6572420f7afd1e714
mfekadu <mike.fekadu@gmail.com>
2019-09-02 22:45:08 -0400

    spawn food randomly with a seed
    also created new globals for the inner game width and height which is different because of the border thickness
    
    line 90 asserts that for this commit, there should be 105 objects on the screen and none of them should disappear even after every update
    
    added a DEBUG variable
    
    random seed!! important!
    
    TODO: remove assertions when further in development ... or factor out into test cases... how??? hmmm...

--- cfg.py
+++ cfg.py
@@ -4,7 +4,14 @@ A place to store global variables to customize and configure the simulation.
 '''
 import colors
 
+SEED = 42  # set the seed for random number generators for reproducability
+
+DEBUG = True  # optionally set to False/0 (no-debug), 1 (asserts), 2 (prints)
+
+FOOD_COUNT = 100
 FOOD_RADIUS = 5
 FOOD_COLOR = colors.RED
+
+
 BOT_RADIUS = 10
 BOT_COLOR = colors.BLUE
--- sim.py
+++ sim.py
@@ -7,8 +7,11 @@ from create_shapes import create_circle, create_rect
 from create_world import draw_border
 from food import create_food
 from bot import create_bot
-from colors import *
 import numpy as np
+import cfg
+
+# set the seed for reproducability
+np.random.seed(cfg.SEED)
 
 
 def get_pymunk_space(gravity=(0, -9.807)):
@@ -23,11 +26,17 @@ def get_pymunk_space(gravity=(0, -9.807)):
 # game width and height
 GW = 400
 GH = 400
+BORDER_THICCNESS = 10
+
 # must be global because the @decorators work that way
 window = pyglet.window.Window(GW, GH, __file__, resizable=False)
 space = get_pymunk_space(gravity=(0, -100))
 options = DrawOptions()
-space.add(draw_border(GW, GH))
+space.add(draw_border(GW, GH, thicc=BORDER_THICCNESS))
+
+# the space that food and bots can spawn
+IGW = GW - (BORDER_THICCNESS * 2)  # inner game width
+IGH = GH - (BORDER_THICCNESS * 2)  # inner game height
 
 
 def run():
@@ -59,9 +68,15 @@ def main():
     assert window
     assert space
 
-    food_body, food_shape = create_food()
-    food_body.position = (30, 30)
-    space.add(food_body, food_shape)
+    # initialize food at random places
+    for i in range(cfg.FOOD_COUNT):
+        food_body, food_shape = create_food()
+        x = np.random.randint(BORDER_THICCNESS, IGW)
+        y = np.random.randint(BORDER_THICCNESS, IGH)
+        food_body.position = (x, y)
+        space.add(food_body, food_shape)
+
+    assert len(space.shapes) == cfg.FOOD_COUNT + 4  # 4 borders and 100 food
 
     bot_body, bot_shape = create_bot()
     bot_body.position = (30, 70)
@@ -69,7 +84,10 @@ def main():
 
     # do the update on 1/60th clock-ticks
     def update(dt):
-        [print(type(shape), shape.body.position) for shape in space.shapes]
+        if cfg.DEBUG > 1:
+            [print(type(shape), shape.body.position) for shape in space.shapes]
+        # 4 borders + 1 bot + 100 food
+        assert len(space.shapes) == cfg.FOOD_COUNT + 5 if cfg.DEBUG else None
         for shape in space.shapes:
             x, y = shape.body.position.x, shape.body.position.y
             if (x < (0-10) or x > (GW+10) or y < (0-10) or y > (GH+10)):
@@ -82,5 +100,6 @@ def main():
 
 
 if __name__ == "__main__":
-    print("hello world")
+    print("DEBUG LEVEL 1 - ASSERT STATEMENTS") if cfg.DEBUG else None
+    print("DEBUG LEVEL 2 - PRINT STATEMENTS") if cfg.DEBUG > 1 else None
     main()
